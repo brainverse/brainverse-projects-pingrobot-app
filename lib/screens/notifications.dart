125701454eeae707071fe90341c26ca056486f79
carl <carl@limatech.co.ke>
2022-03-02 00:54:29 +0300

    initial implementation of notifications screen

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:pingrobot/theme/colors.dart';
+import 'package:pingrobot/util/notifications.dart';
 
-class Notifications extends StatelessWidget {
+class Notifications extends StatefulWidget {
   const Notifications({Key? key}) : super(key: key);
+
+  @override
+  State<Notifications> createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  final List unreadNotifications = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var item in notifications) {
+      if (!item['read']) {
+        unreadNotifications.add(item);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +29,87 @@ class Notifications extends StatelessWidget {
       appBar: AppBar(
         title: Text('Notifications'),
         backgroundColor: CustomColors.primaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+        child: Column(
+          children: [
+            if (unreadNotifications.isNotEmpty)
+              Expanded(
+                child: Card(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 8.0, left: 8.0, bottom: 8.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Unread',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: unreadNotifications.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: Icon(Icons.dns),
+                            title:
+                                Text('${unreadNotifications[index]['title']}'),
+                            subtitle: Text(
+                                '${unreadNotifications[index]['message']}'),
+                            trailing:
+                                Text('${unreadNotifications[index]['date']}'),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            if (unreadNotifications.isNotEmpty)
+              const SizedBox(
+                height: 15,
+              ),
+            Expanded(
+              child: Card(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 8.0, left: 8.0, bottom: 8.0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Read',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: notifications.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: Icon(Icons.web),
+                            title: Text('${notifications[index]['title']}'),
+                            subtitle:
+                                Text('${notifications[index]['message']}'),
+                            trailing: Text('${notifications[index]['date']}'),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
---
+++ lib/util/notifications.dart
@@ -0,0 +1,87 @@
+// fetched by userId
+List notifications = [
+  {
+    'id': 1,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 2,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 3,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 4,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 5,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 6,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': false,
+    'date': 'days ago'
+  },
+  {
+    'id': 7,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 8,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': false,
+    'date': 'days ago'
+  },
+  {
+    'id': 9,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 10,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+  {
+    'id': 11,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': false,
+    'date': 'days ago'
+  },
+  {
+    'id': 12,
+    'title': 'Notification Title',
+    'message': 'Notification message body',
+    'read': true,
+    'date': 'days ago'
+  },
+];
